698b351c94a0147e3ba36a28f52db72782c7dc28
Laurd11 <ldoyle@uw.edu>
2024-02-25 12:23:24 -0500

    commenting initiated / streamlined code

--- code/utils.py
+++ code/utils.py
@@ -2,41 +2,46 @@ import numpy as np
 import torch
 from torch import nn
 import os
-# from torch.utils.data import DataLoader
-# from torchvision import datasets, transforms
 import torch.nn.functional as F
 import torch.optim as optim
 from torch.utils.data import TensorDataset, DataLoader, RandomSampler
 from sklearn.model_selection import train_test_split
 from imblearn.over_sampling import SMOTE
 
+"""
+This file contains functions and classes utilized in data processing and model training
+"""
 
-# def retrieve_images(fil_dir):
-#     sample_list = sorted(os.listdir(fil_dir))
-#     img_list = np.zeros((len(sample_list),7,40,500))
-#     ind = 0
-#     for file_name in sample_list:
-#         if file_name[5:8] != "000":
-#             img_list[ind,:,:,:] = np.load(os.path.join(fil_dir,file_name))
-#             ind+=1
-#     return img_list[:ind,:,:,:]
+
+# BASIC PROCESSED IMAGE LOADING FUNCTIONS
+
 
 def retrieve_images(fil_dir, exclude_clean=False, shape=(7,40,500)):
+    # gets processed multichannel images from a single directory
+    # flexible shape, exclude_clean is used to get defect images only
+    # returns numpy array (# images x #channels x height x width)
+
     sample_list = sorted(os.listdir(fil_dir))
     img_list = np.zeros((len(sample_list),shape[0],shape[1],shape[2]))
     ind = 0
     if exclude_clean:
+        # only retrieves images with defects
         for file_name in sample_list:
             if file_name[5:8] != "000":
                 img_list[ind,:,:,:] = np.load(os.path.join(fil_dir,file_name))
                 ind+=1
     else:
+        # retrieves all images in directory
         for file_name in sample_list:
             img_list[ind,:,:,:] = np.load(os.path.join(fil_dir,file_name))
             ind+=1
     return img_list[:ind,:,:,:]
 
 def retrieve_masks(fil_dir, exclude_clean=False):
+    # gets processed masks from a directory, same exclude_clean option as above
+    # shape is specific to single channel masks corresponding to wavelet filters
+    # returns numpy array (# images x 1 x 40 x 500)
+
     sample_list = sorted(os.listdir(fil_dir))
     img_list = np.zeros((len(sample_list),40,500))
     ind = 0
@@ -51,7 +56,17 @@ def retrieve_masks(fil_dir, exclude_clean=False):
             ind+=1
     return img_list.reshape(-1,1,40,500)[:ind,:,:,:]
 
+
+# DATA METHODS AND MODEL FOR SLIDING WINDOW MODEL
+
+
 def sliding_window(image, sq_size = 22, pix_steps=2):
+    # helper function that takes a multichannel numpy array image and goes left to
+    # right row by row extracting windowed subimages of size sq_size
+    # pix steps is the number of steps taken between each column and each row of windows
+    # when pix_steps<sq_size, the windows overlap
+    # Returns: numpy array of size (#total windows x sq_size x sq_size)
+
     img_list = []
     for i in np.arange(0,image.shape[1]-sq_size+pix_steps,pix_steps):
         for j in np.arange(0,image.shape[2]-sq_size+pix_steps,pix_steps):
@@ -60,6 +75,11 @@ def sliding_window(image, sq_size = 22, pix_steps=2):
 
 
 def get_windowed_data(imgs, masks):
+    # helper function that takes arrays imgs (#images x #chnnels x height x width)
+    # and masks  (#images x 1 x height x width) and applies sliding_window to both.
+    # The sliding windows of imgs are returned (#windows x #channels x 22 x 22)
+    # the labels of each window are returned (#windows x 1)
+
     windows_per_im = 10*240
     fin_data = np.zeros((imgs.shape[0]*windows_per_im,7,22,22))
     fin_labs = np.zeros((imgs.shape[0]*windows_per_im,1))
@@ -72,14 +92,21 @@ def get_windowed_data(imgs, masks):
     return fin_data, fin_labs
         
 class Net(nn.Module):
+    # CNN applied to 7x22x22 windows of wavelet filtered data,
+    # outputs a binary classification. second to last activation used for 
+    # final image classification.
+    
     def __init__(self):
+        # output sizes listed to the right
         super().__init__() # 7 x 22 x 22
         self.conv1 = nn.Conv2d(7, 16, 4,stride=2) # 16 x 10 x 10
         self.pool = nn.MaxPool2d(2,stride=1) # 16 x 9 x 9
         self.conv2 = nn.Conv2d(16, 32, 3,stride=2) # 32 x 4 x 4
+        # flatten vector
         self.drop = nn.Dropout(p=0.15)
         self.fc1 = nn.Linear( 512, 120)   
         self.fc2 = nn.Linear(120, 84)
+        # OUTPUT OF LAYER fc2 USED FOR RESULT MODEL
         self.fc3 = nn.Linear(84, 2)
 
     def forward(self, x):
@@ -93,6 +120,13 @@ class Net(nn.Module):
         return x
 
 def get_processed_data(source_dir, mask_dir, test_split = 0.2,val_split=0.15):
+    # this function takes directories containing all processed images (the 7x40x500 wavelet images
+    # and the 40x500 corresponding masks) and creates sliding window 7x22x22 images and binary 
+    # classification of defect presence (1x2) for each window. The result is 10 * 240 sliding window
+    # results per image. Since there are significantly more defect free images than defect ones, 
+    # imblearn.over_sampling.SMOTE is used to balance the distribution
+
+    # get images and masks from directories, ONLY USES IMAGES WITH DEFECTS via exclude_clean
     xs = retrieve_images(source_dir, exclude_clean=True)
     masks = retrieve_masks(mask_dir, exclude_clean=True)
 
@@ -102,6 +136,7 @@ def get_processed_data(source_dir, mask_dir, test_split = 0.2,val_split=0.15):
     X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=val_split, random_state=81)
     X_train, y_train = get_windowed_data(X_train, y_train)
 
+    # SMOTE used to artificially balance positive/negative sample distribution
     smote = SMOTE(random_state=81)
     X_train, y_train = smote.fit_resample(X_train.reshape(-1, 22*22*7), y_train)
 
@@ -109,9 +144,11 @@ def get_processed_data(source_dir, mask_dir, test_split = 0.2,val_split=0.15):
 
     print("Data balanced")
 
+    # validation data windowing
     X_val, y_val = get_windowed_data(X_val, y_val)
     X_test, y_test = get_windowed_data(X_test, y_test)
 
+    # reshapes the labels for all the windowed sub data sets
     labels = np.zeros((y_train.shape[0],2))
     labels[:,1] = y_train.flatten()
     labels[np.argwhere(y_train==0).flatten(),0] = 1
@@ -126,14 +163,32 @@ def get_processed_data(source_dir, mask_dir, test_split = 0.2,val_split=0.15):
 
     return X_train, X_val, X_test, labels, val_labels, test_labels
 
+
+# TRAINING FUNCTIONS FOR BOTH MODELS
+
+
 def training_procedure(net, X_train, X_val, labels, val_labels, dest_fol, total_epochs = 60, learning_rate=5e-5, sgd_batch = 80000):
+    # basic training procedure for NN, training, and validation sets input. Expects numpy arrays for features and labels
+    # Requires dest_fol file path for outputs of model save points and loss history vectors
+    # returns trained network, training loss vector history, and validation loss vector history
+
+    # Utilizes basic binary Cross Entropy Loss and Adam optimizer as well as fixed batch size of 50
+
+    # An optional SGD_batch size is included for large training sets, such that a random fixed sample size of
+    # the training set can be taken at each epoch in order to make best use of computational resources
+    # If full training set desired, set sgd_batch to None
+
     criterion  = nn.CrossEntropyLoss() 
     optimizer = optim.Adam(net.parameters(), lr=learning_rate)
 
     train_dataset = TensorDataset(torch.from_numpy(X_train).type(torch.FloatTensor), torch.tensor(labels))
-    train_loader = DataLoader(train_dataset,batch_size=50,sampler = RandomSampler(train_dataset, replacement=False,num_samples=sgd_batch)) 
+
+    # this data loader uses RandomSampler to randomize the ordering of the input data for each epoch
+    train_loader = DataLoader(train_dataset,batch_size=50,sampler = RandomSampler(train_dataset, 
+                                                                                  replacement=False,num_samples=sgd_batch)) 
+    
     val_dataset = TensorDataset(torch.from_numpy(X_val).type(torch.FloatTensor), torch.tensor(val_labels))
-    val_loader = DataLoader(val_dataset, batch_size=50)#,sampler = RandomSampler(train_dataset, replacement=False, num_samples=30000))  
+    val_loader = DataLoader(val_dataset, batch_size=50)  
     
     print("Training Initiated")
 
@@ -153,12 +208,15 @@ def training_procedure(net, X_train, X_val, labels, val_labels, dest_fol, total_
             optimizer.step()
 
             running_loss += loss.item()
+
+        # validation loss 
         net.eval()
         with torch.no_grad():
             for batch, (val_inputs, val_labs) in enumerate(val_loader):
                 val_outputs = net(val_inputs)
                 valloss = criterion(val_outputs, val_labs)
                 val_losses[epoch] +=valloss.item()
+
         if epoch % 5==0:
             print("Epoch "+str(epoch+1)+": \n Training Loss is: "+str(running_loss))
             print(" Validation Loss is: "+str(val_losses[epoch] ))
@@ -168,7 +226,12 @@ def training_procedure(net, X_train, X_val, labels, val_labels, dest_fol, total_
 
     return net, losses, val_losses
 
+
 def final_test(net, X_test,y_test):
+    # basic binary classification accuracy assessment, prints results from a numpy
+    # array test set given for the NN provided. Assumes y_test is (#samples x 2)
+    # with column 0 as negative results and column 1 as positives
+
     net.eval()
     test_dataset = TensorDataset(torch.from_numpy(X_test).type(torch.FloatTensor), torch.tensor(y_test))
     test_loader = DataLoader(test_dataset, batch_size=50)
@@ -179,20 +242,38 @@ def final_test(net, X_test,y_test):
         for batch, (val_inputs, val_labs) in enumerate(test_loader):
             val_outputs = net(val_inputs).detach().numpy()
             val_outputs = (val_outputs[:,1]>val_outputs[:,0]).astype(int)
+
+            # number of true positives in batch
             num_correct += np.sum(val_labs.numpy()[:,1].flatten() * val_outputs.flatten())
+
+            # number of true negatives in batch
             true_neg += np.sum(val_labs.numpy()[:,0].flatten() * (1-val_outputs.flatten()))
-            # num_correct+=np.argwhere([:,1] ==val_labs.numpy()[:,1]).flatten().shape[0]
+
     acc = num_correct / np.sum(y_test[:,1])
     tn_rate = true_neg / np.sum(y_test[:,0])
+    full_acc = (num_correct+true_neg) / y_test.shape[0]
+
     print("Sensitivity: "+str(acc))
     print("Specificity: "+str(tn_rate))
+    print("Full Accuracy: "+str(full_acc))
+
+
+
+# RESULT MODEL TRAINING
+
 
 class Net_result(nn.Module):
+    # CNN that takes activation output from the windowed model (84channels x 10height 
+    # x 240width) and produces a binary classification result (2 element probabilities) 
+    # for the whole image
+
     def __init__(self):
-        super().__init__() # 84 x 10 x 240
+        # output sizes listed to the right
+        super().__init__() # input: 84 x 10 x 240
         self.conv1 = nn.Conv2d(84, 32, (1,2),stride=(1,2)) # 32 x 10 x 120 
         self.pool = nn.MaxPool2d(2) # 32 x 5 x 60
         self.conv2 = nn.Conv2d(32, 16, 2,stride=(1,2)) # 16 x 4 x 30 
+        # flatten vector
         self.drop = nn.Dropout(p=0.15)
         self.fc1 = nn.Linear( 1920, 360)   
         self.fc2 = nn.Linear(360, 120)
@@ -201,6 +282,7 @@ class Net_result(nn.Module):
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
         x = F.relu(self.conv2(x))
+
         x = torch.flatten(x,1) 
         x = self.drop(x)
         x = F.relu(self.fc1(x))
@@ -209,8 +291,15 @@ class Net_result(nn.Module):
         return x
     
 def get_windowed_output(source_dir, test_split = 0.2, val_split=0.15):
+    # function that fetches outputs of the initial windowed model from a path
+    # source_dir and outputs training, validation, and test sets from this data
+    # optional percentages 
+
+    # xs is #images x 84 channels x 10height x 240width
     xs = retrieve_images(source_dir, shape=(84,10,240))
     name_list = sorted(os.listdir(source_dir))
+
+    # this gets the image classifications from the file names
     ys = [1. if el[5:8]=="000" else 0. for el in name_list]
     ys = np.array(ys).reshape((-1,1))
     ys = np.hstack((ys, 1-ys))
--- code/windowed_output.py
+++ code/windowed_output.py
@@ -4,17 +4,26 @@ import os
 
 from utils import *
 
+"""
+This script is run to bridge the training between the window model and the result image model.
+It extracts the 84 channel activation outputs for each image from the first using a hook on 
+the window model and then saves all the results in dest_dir.
+"""
+
 source_dir = input("Enter directory with processed wavelet images: ") 
 model_loc = input("Enter file path of model weights ('.pt' or 'pth'): ") 
 dest_dir = input("Enter desired directory for model outputs: ") 
 
+# collect all the images and their names
 img_list = retrieve_images(source_dir)
 name_list = sorted(os.listdir(source_dir))
 
+# load model with desired trained weights
 net = Net()
 net.load_state_dict(torch.load(model_loc))
 net.eval()
 
+# set forward hook
 activations = []
 def retrieveActivations():
     def hook(model, input, output):
@@ -23,7 +32,7 @@ def retrieveActivations():
 
 hook_layer = net.fc2.register_forward_hook(retrieveActivations())
 
-
+# run through all images, apply sliding window, and store the window activations using the hook
 for el in range(len(name_list)):
     im_win, _ = get_windowed_data(img_list[el,:,:,:].reshape(1,7,40,500),np.zeros(((1,1,40,500))))
     im_win = torch.from_numpy(im_win).type(torch.FloatTensor)
@@ -31,6 +40,8 @@ for el in range(len(name_list)):
 
 hook_layer.remove()
 
+# save the activations in an appropriately named numpy array on disk
+# 84 channel vectors for each window (there are 10 x 240 per image) are stored
 for el in range(len(activations)):
     outs_np = activations[el].transpose().reshape((84,10,240))
     np.save(os.path.join(dest_dir,name_list[el]),outs_np)
